6d5bef173f4a60808ef7ef4fd020cd5887a519d5
1ambd4 <dr.chen@mail.ustc.edu.cn>
2024-02-28 13:53:36 +0800

    ingore format on save for specify filetype

--- lua/custom/configs/conform.lua
+++ lua/custom/configs/conform.lua
@@ -14,15 +14,31 @@ local options = {
       yaml = { "prettierd" },
       markdown = { "prettierd" },
       graphql = { "prettierd" },
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ["*"] = { "codespell" },
+      -- Use the "_" filetype to run formatters on filetypes
+      -- that don't have other formatters configured.
+      ["_"] = { "trim_whitespace" },
     },
 
-    format_on_save = {
-      -- pattern = '.lua,*.graphql,*.css,*.html,*.json,*.yaml,*.md,*.gql,*.rs',
-      pattern = "*.lua,*.rs,*.go,*.ts,*.js,*.tsx,*.jsx",
-      timeout_ms = 500,
-      async = false,
-      lsp_fallback = true,
-    },
+    -- format_on_save = {
+    --   -- pattern = '.lua,*.graphql,*.css,*.html,*.json,*.yaml,*.md,*.gql,*.rs',
+    --   pattern = "*.lua,*.rs,*.go,*.ts,*.js,*.tsx,*.jsx",
+    --   timeout_ms = 500,
+    --   async = false,
+    --   lsp_fallback = true,
+    -- },
+
+    format_on_save = function(bufnr)
+      local ignore_filetypes = { "cpp", "java" }
+      if not vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+        return {
+          timeout_ms = 500,
+          async = false,
+          lsp_fallback = true,
+        }
+      end
+    end,
   },
 }
 
